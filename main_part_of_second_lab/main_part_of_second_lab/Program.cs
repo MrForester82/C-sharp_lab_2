cbfb889a400bb145985f1cd2918434c9aeedaf3f
MrForester82 <potate82@ya.ru>
2017-02-09 22:51:52 +0300

    часть 3

--- main_part_of_second_lab/main_part_of_second_lab/Program.cs
+++ main_part_of_second_lab/main_part_of_second_lab/Program.cs
@@ -53,6 +53,111 @@ namespace main_part_of_second_lab
             //Console.WriteLine(i);
 
             //часть 3 -----------------------------------------------------------------------------------------------------
+
+            /*  3.1
+            Console.Write("x = ");
+            float x = float.Parse(Console.ReadLine());
+            Console.Write("y = ");
+            float y = float.Parse(Console.ReadLine());
+            if (x > y )
+                Console.WriteLine("ax = "+x);
+            else
+                Console.WriteLine("max = "+y);
+            */
+
+            /*  3.2
+            int Ball=0;
+            Console.Write("x = ");
+            float x = float.Parse(Console.ReadLine());
+            Console.Write("y = ");
+            float y = float.Parse(Console.ReadLine());
+            if (x * x + y * y <=1)
+                Ball = 10;	            //окружность с радиусом 1
+            else
+                if (x * x + y * y <= 4)
+                    Ball = 5;	        //окружность с радиусом 2
+                else
+                    if (x * x + y * y <= 9)
+                        Ball = 1;       //окружность с радиусом 3
+            Console.WriteLine("Ball= "+ Ball);
+            */
+
+            /*  3.3
+            Console.Write("OPER = ");
+            char oper = char.Parse(Console.ReadLine());
+            bool ok = true;
+            Console.Write("A = ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("B = ");            
+            int b = int.Parse(Console.ReadLine());
+            float res = 0;
+            switch (oper)
+            {
+                case '+': res = a + b; goto case '-';// break;	//1
+                case '-': res = a - b; break;
+                case '*': res = a * b; break;
+                case ':': case'/':
+                if(b != 0)
+                //{
+                    res = (float)a / b; break;
+                //}
+            //else
+            //        goto default;
+            default: ok = false; break;        
+            }
+            if(ok)
+                Console.WriteLine("{0} {1} {2} = {3}", a, oper, b, res);
+            else
+                Console.WriteLine("error");
+            */
+
+            /*  3.4
+            Console.Write("N = ");
+            int n = int.Parse(Console.ReadLine());
+            int i = n;
+            while (i >= 1)                 
+            {
+                if(i%2 != 0)
+                    Console.Write(" " + i);
+                --i;
+            }
+            Console.Write("\n");
+            */
+
+            /*  3.5
+            Console.Write("N = ");
+            int n = int.Parse(Console.ReadLine());
+            int i = n;
+            do
+            {
+                if (i % 2 == 0)
+                    Console.Write(" " + i);
+                --i;
+            } while (i >= 1);
+            Console.Write("\n");
+            */
+
+            /*  3.6
+           Console.Write("N = ");
+           int n = int.Parse(Console.ReadLine());
+           for(int i=n; i>=1; i--)
+           {
+            Console.Write(" " + i*i);
+           }
+           Console.Write("\n");
+           */
+
+             /*  3.7
+            Console.Write("M = ");
+            int m = int.Parse(Console.ReadLine());
+            Console.Write("N = ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= m; ++i, Console.WriteLine())     //1
+                for (int j=1; j<=n; ++j)
+                    Console.Write("\t" + i*j); 
+            */
+
+
         }
     }
 }
