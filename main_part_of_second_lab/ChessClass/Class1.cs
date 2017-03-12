65f6de69fc1159ec8b5b2874b0e918f55ab0e9bc
MrForester82 <potate82@ya.ru>
2017-03-12 10:30:28 +0300

    ещё намного работы

---
+++ main_part_of_second_lab/ChessClass/Class1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessClass
+{
+    public class ChessBoard
+    {
+        private int line = 0;
+        private int column = 0;
+
+        public ChessBoard(int line, int column)
+        {
+            if (line > 3 && column > 3)
+            {
+                this.line = line;
+                this.column = column;
+            }
+            else
+            {
+                Console.WriteLine("Некорректный размер шахматного поля!!");
+            }
+        }
+
+        public bool isCorrect()
+        {
+            if (line > 3 && column > 3)
+                return true;
+            else
+                return false;
+        }
+
+        public int countOfBlack()
+        {
+            if (line % 2 == 0)
+                return line * column / 2;
+            else
+                return (line * column / 2) + 1;
+        }
+
+        public string Color(int line, int column)
+        {
+            if ((line + column) % 2 == 0)
+                return "чёрный";
+            else
+                return "белый";
+        }
+
+        public bool isSimilar(int l1, int c1, int l2, int c2)
+        {
+            if ((l1 + c1) % 2 == 0 && (l2 + c2) % 2 == 0)
+                return true;
+            else if ((l1 + c1) % 2 != 0 && (l2 + c2) % 2 != 0)
+                return true;
+            return false;
+        }
+
+        public bool isUnderAttack(int l1, int c1, int l2, int c2, char figure)
+        {//p - пешка    e - слон    l - ладья   f - ферзь   h - конь
+            bool f = false;
+            int lt = l2;
+            int ct = c2;
+            switch (figure)
+            {
+                case 'p':
+                    if ((l1 == l2 - 1 && c1 == c2 - 1) || (l1 == l2 - 1 && c1 == c2 + 1))
+                        return true;
+                    else
+                        return false;
+
+                case 'e':
+                    if (l1 == l2 || c1 == c2)
+                        return true;
+                    else
+                        return false;
+
+                case 'l':
+                    while (f == false && lt >= 1 && ct <= 1)
+                    {
+                        lt--;
+                        ct--;
+                        if (l1 == lt && c1 == ct)
+                            return true;
+                    }
+                    lt = l2;
+                    ct = c2;
+                    while (f == false && lt <= line && ct <= column)
+                    {
+                        lt++;
+                        ct++;
+                        if (l1 == lt && c1 == ct)
+                            return true;
+                    }
+                    lt = l2;
+                    ct = c2;
+                    while (f == false && lt <= line && ct >= 1)
+                    {
+                        lt++;
+                        ct--;
+                        if (l1 == lt && c1 == ct)
+                            return true;
+                    }
+                    lt = l2;
+                    ct = c2;
+                    while (f == false && lt >= 1 && ct <= column)
+                    {
+                        lt--;
+                        ct++;
+                        if (l1 == lt && c1 == ct)
+                            return true;
+                    }
+                    return false;
+
+                case 'f':
+                    while (f == false && lt >= 1 && ct <= 1)
+                    {
+                        lt--;
+                        ct--;
+                        if (l1 == lt && c1 == ct)
+                            f = true;
+                    }
+                    lt = l2;
+                    ct = c2;
+                    while (f == false && lt <= line && ct <= column)
+                    {
+                        lt++;
+                        ct++;
+                        if (l1 == lt && c1 == ct)
+                            f = true;
+                    }
+                    lt = l2;
+                    ct = c2;
+                    while (f == false && lt <= line && ct >= 1)
+                    {
+                        lt++;
+                        ct--;
+                        if (l1 == lt && c1 == ct)
+                            f = true;
+                    }
+                    lt = l2;
+                    ct = c2;
+                    while (f == false && lt >= 1 && ct <= column)
+                    {
+                        lt--;
+                        ct++;
+                        if (l1 == lt && c1 == ct)
+                            f = true;
+                    }
+                    if (f == true || (l1 == l2 || c1 == c2))
+                        return true;
+
+                    else
+                        return false;
+
+                case 'h':
+                    if ((l1 == l2 - 1 && c1 == c2 + 2) || (l1 == l2 - 2 && c1 == c2 + 1) || (l1 == l2 - 2 && c1 == c2 - 1) || (l1 == l2 - 1 && c1 == c2 - 2) || (l1 == l2 + 1 && c1 == c2 + 2) || (l1 == l2 + 2 && c1 == c2 + 1) || (l1 == l2 + 2 && c1 == c2 - 1) || (l1 == l2 + 2 && c1 == c2 - 2))
+                        return true;
+                    else
+                        return false;
+
+                default:
+                    Console.WriteLine("Ошибка выбора фигуры");
+                    return false;
+            }
+
+        }
+    }
+}
--- main_part_of_second_lab/main_part_of_second_lab/Program.cs
+++ main_part_of_second_lab/main_part_of_second_lab/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ChessClass;
 
 namespace main_part_of_second_lab
 {
@@ -60,7 +61,7 @@ namespace main_part_of_second_lab
             Console.Write("y = ");
             float y = float.Parse(Console.ReadLine());
             if (x > y )
-                Console.WriteLine("ax = "+x);
+                Console.WriteLine("max = "+x);
             else
                 Console.WriteLine("max = "+y);
             */
@@ -155,6 +156,131 @@ namespace main_part_of_second_lab
             for (int i = 1; i <= m; ++i, Console.WriteLine())     //1
                 for (int j=1; j<=n; ++j)
                     Console.Write("\t" + i*j); 
+            */
+
+            /*
+            int n;
+            Console.Write("Введите размер шахматного поля: ");
+            n = int.Parse(Console.ReadLine());
+            ChessBoard chess = new ChessBoard(n, n);
+
+            Console.WriteLine("Количество чёрных клеток: {0}", chess.countOfBlack());
+            Console.WriteLine("Цвет клетки 2,3: {0}", chess.Color(2, 3));
+            if (chess.isSimilar(2, 3, 1, 2))
+                Console.WriteLine("Клетки 2,3 и 1,2 одинакового цвета");
+            else
+                Console.WriteLine("Клетки 2,3 и 1,2 различного цвета");
+
+            if (chess.isUnderAttack(4, 3, 2, 3, 'f'))
+                Console.WriteLine("Фигура 4,3 находится под ударом фигуры 2,3 ");
+            else
+                Console.WriteLine("Фигура 4,3 не находится под ударом фигуры 2,3 ");
+            */
+
+            /*
+            DateTime today = DateTime.Today;
+            Console.WriteLine(today.ToShortDateString());
+            DateTime begyear = new DateTime(2017,01,01);
+            var a = today.Subtract(begyear).Days;
+            Console.WriteLine("Дней с начала года {0}",a);
+            a = today.Subtract(new DateTime(2017,12,31)).Days;
+            Console.WriteLine("Дней до конца года {0}", a*-1);
+            Console.WriteLine("Дата предыдущего дня {0}", today.AddDays(-1).ToShortDateString());
+            Console.WriteLine("Дата следующего дня {0}", today.AddDays(1).ToShortDateString());
+            */
+
+            /*
+            int n = 100;
+            while (n < 1000)
+            {
+                if (Math.Pow((n % 10), 3) + Math.Pow((n % 100) / 10, 3) + Math.Pow(n / 100, 3) == n)
+                    Console.WriteLine(n);
+                n++;
+            }
+            */
+
+            /*
+            Console.WriteLine("Введите размеры сторон прямоугольника, выраженные целыми числами");
+            int m = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+            while (m >= 1 && n >= 1)
+            {
+                if (m > n)
+                    m -= n;
+                if (m < n)
+                    n -= m;
+                if (m == n)
+                {
+                    count++;
+                    break;
+                }
+                count++;
+                Console.WriteLine("m {0}    n {1}   count {2}", m, n, count);
+            }
+            Console.WriteLine("Указанный прямоугольник можно раделить на {0} квадратов", count);
+            */
+
+            /*
+            Console.Write("Введите площадь окружности: ");
+            int s = int.Parse(Console.ReadLine());
+            Console.WriteLine("Радиус окружности: {0}", Math.Sqrt(s)/Math.PI);
+            */
+
+            /*
+            Console.Write("Введите трёхзначное число: ");
+            int numb = int.Parse(Console.ReadLine());
+            if (numb % 10 < numb / 100)
+                Console.WriteLine("Первая цифра больше третьей");
+            else
+                Console.WriteLine("Третья цифра больше первой");
+            */
+
+            /*
+            Console.Write("Введите x: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Введите y: ");
+            int y = int.Parse(Console.ReadLine());
+            if(x<=70 && y>=0 && x<=y)
+                Console.WriteLine("Данная точка входит в область");
+            else
+                Console.WriteLine("Данная точка не входит в область");
+            */
+
+            /*
+            Console.Write("Введите оценку: ");
+            int x = int.Parse(Console.ReadLine());
+            if(x<=100 && x>=90)
+                Console.WriteLine("     Отлично");
+            else if (x <= 89 && x >= 70)
+                Console.WriteLine("     Хорошо");
+            else if (x <= 69 && x >= 50)
+                Console.WriteLine("     Удовлетворительно");
+            else if (x <= 49)
+                Console.WriteLine("     Неудовлетворительно");
+            else
+                Console.WriteLine("     Неверно заданная оценка");
+            */
+
+            /*
+            int bot = 10;
+            while (bot < 100)
+            {
+                if(bot%10 != bot/10)
+                    Console.WriteLine(bot);
+                bot++;
+            }
+            */
+
+           /*
+            for(int i = 4; i >= 0; i--)
+            {
+                for(int j = 0; j <= i; j++)
+                {
+                    Console.Write("  {0}", j);
+                }
+                Console.WriteLine();
+            }
             */
 
 
